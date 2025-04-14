59c8552b33a4c05ea230dc40b481fc9b3d3f1cdf
Nicolás Suárez <nasuarezro@unal.edu.co>
2025-04-14 01:46:17 -0500

    Add authentication routes to the main application

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import workersRoutes from "./routes/workers.routes.js";
 import requestRoutes from "./routes/requests.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
+// Rutas
+app.use(authRoutes);
 app.use(workersRoutes);
 app.use(requestRoutes);
 
